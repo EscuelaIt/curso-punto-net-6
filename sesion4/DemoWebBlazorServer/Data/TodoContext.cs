3c8e494cf20908782fa0832e1fce3542da60ffc4
eiximenis <etomas@gmail.com>
2022-03-02 17:57:55 +0100

    sesion4

---
+++ sesion4/DemoWebBlazorServer/Data/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DemoWebBlazorServer.Data
+{
+    public class TodoContext : DbContext
+    {
+
+        public DbSet<TodoTask> Todos { get; set; }
+
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ sesion4/DemoWebBlazorServer/Data/TodoTask.cs
@@ -0,0 +1,14 @@
+﻿namespace DemoWebBlazorServer.Data
+{
+    public class TodoTask
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+
+        public bool Important { get; set; }
+
+        public DateTime? DoneWhen { get; set; }
+
+        public bool IsDone { get => DoneWhen.HasValue; }
+    }
+}
